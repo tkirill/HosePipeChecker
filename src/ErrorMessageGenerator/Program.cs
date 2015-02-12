0e26a0d364b8634a217c424f18c866f917148049
Kirill Teplinskiy <tkirill128@gmail.com>
2015-02-12 23:35:55 +0500

    Add draft of error message generator.
    This is a tool for fill default EasyNetQ error queue.  It will be useful for
    generating test data.

---
+++ src/ErrorMessageGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using EasyNetQ;
+
+namespace ErrorMessageGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                using (var processed = new AutoResetEvent(false))
+                {
+                    bus.Subscribe<Message>("test", m =>
+                    {
+                        processed.Set();
+                        throw new Exception("error");
+                    });
+
+                    var msg = new Message { Field1 = "test" };
+                    bus.Publish(msg);
+                    processed.WaitOne();
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }
+        }
+    }
+
+    public class Message
+    {
+        public string Field1 { get; set; }
+    }
+}
