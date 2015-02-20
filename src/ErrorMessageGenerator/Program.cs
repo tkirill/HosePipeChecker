dc73aaa59a50e45543cf95549de8086f1d5eeff3
Kirill Teplinskiy <tkirill128@gmail.com>
2015-02-20 23:56:36 +0500

    Add a bunch of options to ErrorMessageGenerator

--- src/ErrorMessageGenerator/Program.cs
+++ src/ErrorMessageGenerator/Program.cs
@@ -1,34 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using CommandLine;
+using CommandLine.Text;
 using EasyNetQ;
 
 namespace ErrorMessageGenerator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            var options = new Options();
+            if (Parser.Default.ParseArguments(args, options))
             {
-                using (var processed = new AutoResetEvent(false))
+                using (var bus = RabbitHutch.CreateBus(GetConnectionString(options)))
                 {
-                    bus.Subscribe<Message>("test", m =>
+                    using (var processed = new CountdownEvent(options.Count))
                     {
-                        processed.Set();
-                        throw new Exception("error");
-                    });
+                        bus.Subscribe<Message>("test", m =>
+                        {
+                            if (!processed.IsSet)
+                            {
+                                processed.Signal();
+                                throw new Exception("error");
+                            }
+                        });
+                        
+                        for (var i = 0; i < options.Count; i++)
+                        {
+                            var msg = new Message {Field1 = "test", Field2 = 10};
+                            bus.Publish(msg);
+                        }
 
-                    var msg = new Message { Field1 = "test" };
-                    bus.Publish(msg);
-                    processed.WaitOne();
-                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                        processed.Wait(TimeSpan.FromMinutes(2));
+                        Console.WriteLine("Processed");
+                        Thread.Sleep(TimeSpan.FromSeconds(10));
+                        Console.WriteLine("Done");
+                    }
                 }
             }
+        }
+
+        private static string GetConnectionString(Options options)
+        {
+            var parts = new List<string>();
+
+            parts.Add(string.Format("host={0}", options.Host));
+            if (!string.IsNullOrEmpty(options.User))
+                parts.Add(string.Format("username={0}", options.User));
+            if (!string.IsNullOrEmpty(options.Password))
+                parts.Add(string.Format("password={0}", options.Password));
+
+            return string.Join(";", parts);
+        }
+    }
+
+    public class Options
+    {
+        [Option('h', "host", DefaultValue = "localhost", HelpText = "RabbitMQ host")]
+        public string Host { get; set; }
+
+        [Option('u', "user", HelpText = "RabbitMQ user")]
+        public string User { get; set; }
+
+        [Option('p', "password", HelpText = "RabbitMQ password")]
+        public string Password { get; set; }
+
+        [Option('c', "count", DefaultValue = 10, HelpText = "Number of errors")]
+        public int Count { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, x => HelpText.DefaultParsingErrorsHandler(this, x));
         }
     }
 
     public class Message
     {
         public string Field1 { get; set; }
+        public int Field2 { get; set; }
     }
 }
